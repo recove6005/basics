5466c18f764956f15011200986c4757217ac383b
recove6005 <135129542+recove6005@users.noreply.github.com>
2024-03-01 17:15:39 +0900

    Delete html, css directory

--- html, css/0620/댓글/댓글.js
+++
@@ -1,6 +0,0 @@
-const textarea = document.getElementById('area-contents').querySelector('textarea');
-const textareaTextCount = document.querySelector('#area-contents > div > span');
-
-textarea.oninput = () => {
-    textareaTextCount.textContent = textarea.value.length + ' / 100';
-}
